68cb4813f8491e4b0704063b0e814afd264615c0
stanly888 <a0918853297@icloud.com>
2025-05-19 18:51:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     lineConfig: {
-        channelAccessToken: 'YOUR_LINE_CHANNEL_ACCESS_TOKEN',
-        channelSecret: 'YOUR_LINE_CHANNEL_SECRET'
+        channelAccessToken: '9PZB8vPYyUIjzRGS0Kqq3+5p74feKtXRNKBu/e5nlS2YDX+KjIHfnHIPf+H7RzNAeHE2apStmAHD6WMmTKH7+whGr80fnHVD7e8vRdkpW0lKwRxdzmPR2lb0Gf38AtNbwpWCVHfpEyTaT6Aoqqe4NwdB04t89/1O/w1cDnyilFU=',
+        channelSecret: '88b67099b66209c5e85130643544513e'
     },
     sheetId: '1CvpVuPu65x6DROEqj2mXDamKt3iANL7wYYx6U0bp1Yg'
 };
