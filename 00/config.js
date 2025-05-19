931da954d9225a113ff2fc27601908b7540c2393
stanly888 <a0918853297@icloud.com>
2025-05-19 18:45:46 +0800

    Delete 00 directory

--- 00/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    lineConfig: {
-        channelAccessToken: 'YOUR_LINE_CHANNEL_ACCESS_TOKEN',
-        channelSecret: 'YOUR_LINE_CHANNEL_SECRET'
-    },
-    sheetId: '1CvpVuPu65x6DROEqj2mXDamKt3iANL7wYYx6U0bp1Yg'
-};
